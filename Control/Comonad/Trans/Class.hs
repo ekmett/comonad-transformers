1262ae945578d8a74441963ec8771427f2d8eb68
Edward Kmett <ekmett@gmail.com>
2011-01-05 03:25:49 -0500

    repo initialized

---
+++ Control/Comonad/Trans/Class.hs
@@ -0,0 +1,21 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Comonad.Trans.Class
+-- Copyright   :  (C) 2008-2011 Edward Kmett
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  Edward Kmett <ekmett@gmail.com>
+-- Stability   :  experimental
+-- Portability :  portable
+----------------------------------------------------------------------------
+module Control.Comonad.Trans.Class (ComonadTrans(..)) where
+
+import Control.Comonad
+import Control.Monad.Trans.Identity
+
+class ComonadTrans t where
+  lower :: Comonad w => t w a -> w a 
+
+-- avoiding orphans
+instance ComonadTrans IdentityT where
+  lower = runIdentityT
---
+++ Control/Comonad/Trans/Identity.hs
@@ -0,0 +1,30 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Comonad.Trans.Identity
+-- Copyright   :  (C) 2008-2011 Edward Kmett
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  Edward Kmett <ekmett@gmail.com>
+-- Stability   :  provisional
+-- Portability :  portable
+-- 
+----------------------------------------------------------------------------
+module Control.Comonad.Trans.Identity
+  ( IdentityT(..)
+  ) where
+
+import Control.Monad.Trans.Identity
+
+-- Provided by Control.Comonad to avoid an orphan
+{-
+instance Comonad w => Comonad (IdentityT w) where
+  extract = extract . runIdentityT
+  extend f (IdentityT m) = IdentityT (extend (f . IdentityT) m)
+-}
+
+-- Provided by Control.Comonad.Trans.Class to avoid an orphan
+{-
+instance ComonadTrans IdentityT where
+  colift = IdentityT
+-}
